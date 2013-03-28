b537ee6fc1964546cd2412c54d7fc38e02f97d80
Roc Vallès <vallesroc@gmail.com>
2013-03-28 22:29:42 +0000

    hex2bin: Removed dead code.

--- hex2bin.py
+++ hex2bin.py
@@ -3,15 +3,8 @@ import sys
 fd=open(sys.argv[1])
 decoded=""
 buf=fd.read()
-#print buf
-def split_by_n(seq, n):
-	while seq:
-		yield seq[:n]
-		seq = seq[n:]
-#decoded = str(list(split_by_n(buf,2)))
 decoded=[chr(int(buf[i:i+2],16)) for i in range(0, len(buf), 2)]
 fd.close()
 out=sys.stdout
 out.write(''.join(decoded))
 out.close()
-
